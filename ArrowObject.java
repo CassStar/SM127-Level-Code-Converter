409af10f799336de3f9c0b7a96984aae81379296
CassStar <163167577+CassStar@users.noreply.github.com>
2024-04-22 20:06:13 -0500

    Add files via upload

---
+++ ArrowObject.java
@@ -0,0 +1,11 @@
+package objects;
+
+import level.LevelObject;
+
+public class ArrowObject extends LevelObject {
+
+	public ArrowObject(String data) throws Exception {
+		super(data);
+	}
+
+}
---
+++ BobOmbObject.java
@@ -0,0 +1,11 @@
+package objects;
+
+import level.LevelObject;
+
+public class BobOmbObject extends LevelObject {
+
+	public BobOmbObject(String data) throws Exception {
+		super(data);
+	}
+
+}
